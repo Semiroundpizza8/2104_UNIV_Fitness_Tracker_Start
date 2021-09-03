d8a9a8ff87d33dd5665cfc059895ea7e1e831dc5
Maribel Salas <maribelsalas1293@gmail.com>
2021-09-02 21:40:26 -0500

    added code to dropTables and createTables

--- db/seedData.js
+++ db/seedData.js
@@ -3,24 +3,87 @@
 const { client } = require("./client");
 
 async function dropTables() {
-  console.log('Dropping All Tables...');
-  // refresh and delete anything in our database -> clears out our database
-  // drop all tables, in the correct order
+  try {
+    console.log("Dropping All Tables...");
+    // drop all tables, in the correct order
+    await client.query(`
+      DROP TABLE IF EXISTS routine_activities;
+      DROP TABLE IF EXISTS routines;
+      DROP TABLE IF EXISTS activities;
+      DROP TABLE IF EXISTS users;
+    `);
+    console.log("Finished dropping tables");
+  } catch (error) {
+    console.log("Error while dropping tables");
 
+    throw error;
+  }
 }
+  // refresh and delete anything in our database -> clears out our database
+ 
+
+
 
 async function createTables() {
-  console.log("Starting to build tables...");
+  try {
+    console.log("Starting to build tables...");
+
+    await client.query(`
+      CREATE TABLE users(
+        id SERIAL PRIMARY KEY,
+        username	VARCHAR(255)	UNIQUE NOT NULL,
+        password	VARCHAR(255)	NOT NULL
+      );
+    `);
+    
+    await client.query(`
+      CREATE TABLE activities(
+        id SERIAL PRIMARY KEY,
+        name	VARCHAR(255)	UNIQUE NOT NULL,
+        description TEXT NOT NULL
+    );
+  `);
+
+    await client.query(`
+      CREATE TABLE routines(
+        id SERIAL PRIMARY KEY,
+        "creatorId" INTEGER REFERENCES users(id),
+        "isPublic"	BOOLEAN	DEFAULT false,
+        name	VARCHAR(255)	UNIQUE NOT NULL,
+        goal	TEXT NOT NULL
+    );
+  `);
+
+    await client.query(`
+      CREATE TABLE routine_activities(
+        id	SERIAL	PRIMARY KEY,
+        "routineId" INTEGER REFERENCES routines(id),
+        "activityId" INTEGER REFERENCES activities(id),
+        duration	INTEGER,
+        count	INTEGER,
+        UNIQUE ("routineId", "activityId")
+
+    );
+  `);
+
+    console.log('Finished constructing table!');
+  } catch (error) {
+    console.error('Error constructing tables!');
+
+    throw error;
+  }
+}
+    
+
+
   // create all tables, in the correct order
   // creating the empty templates in our database
 
   // tell our fitness-dev database to make a Users table
 
-  // definte an activities table
+  // define an activities table
 
 
-
-}
 
 /* 
 
